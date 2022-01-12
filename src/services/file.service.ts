a4abbec6bd9669420901ed2684493d0ca757fd22
Alexander Hunter <icehuntmen@gmail.com>
2022-01-12 18:52:36 +0300

    Загрузка файлов для клиента и компании

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 import { Core } from 'crm-core';
@@ -93,40 +93,53 @@ export class FileService {
   }
 
   async documentsClientsUpload(data: {
+    owner: string;
     client: string;
     files: any;
     bucketName: string;
+    comment: string;
   }) {
-    let clientData = await this.clientModel
-      .findOne({ _id: data.client })
-      .exec();
-    const respo = data.files.forEach((files) => {
-      const file = Buffer.from(files.buffer.data).toString('base64');
-      this.gridfs
-        .uploadFileString(
-          file,
-          files.originalname,
-          files.mimetype,
-          {
-            filename: files.originalname,
-            mimetype: files.mimetype,
-            size: files.size,
-          },
-          data.bucketName,
-        )
-        .then(async (resultFile) => {
-          for (const [k, v] of Object.entries(
-            JSON.parse(JSON.stringify(resultFile)),
-          )) {
-            if (k === '_id') {
-              clientData.attachments.set('id', v);
+    let clientData, uploaded, resultFile;
+    try {
+      clientData = await this.clientModel.findOne({ _id: data.client }).exec();
+      if (!clientData) {
+        throw new NotFoundException('Не найден такой контакт (клиент)');
+      }
+      uploaded = data.files.forEach((files) => {
+        const file = Buffer.from(files.buffer.data).toString('base64');
+        this.gridfs
+          .uploadFileString(
+            file,
+            files.originalname,
+            files.mimetype,
+            {
+              owner: data.owner,
+              dateCreate: new Date(),
+              comments: data.comment,
+              filename: files.originalname,
+              mimetype: files.mimetype,
+              size: files.size,
+            },
+            data.bucketName,
+          )
+          .then(async (resultFile) => {
+            for (const [k, v] of Object.entries(
+              JSON.parse(JSON.stringify(resultFile)),
+            )) {
+              if (k === '_id') {
+                clientData.attachments.set('id', v);
+              }
             }
-          }
-          await clientData.save();
-        })
-        .catch((reason) => console.log());
-      return clientData.attachments;
-    });
-    return Core.ResponseDataAsync('add attachment file', respo);
+            await clientData.save();
+          })
+          .catch((reason) => console.log());
+        return clientData.attachments;
+      });
+      resultFile = Core.ResponseDataAsync('add attachment file', uploaded);
+    } catch (e) {
+      resultFile = Core.ResponseError(e.message, e.status, e.error);
+    }
+
+    return resultFile;
   }
 }
